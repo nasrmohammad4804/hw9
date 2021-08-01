a275d4e8699d520b7e9a1f5da375e55a7c4fbc9a
mohammad nasr <nasrmohammad4804@gmail.com>
2021-08-01 14:07:39 +0430

    create class person with name and firstName and can sort

---
+++ hw9/task4/src/Person.java
@@ -0,0 +1,44 @@
+
+public class Person implements Comparable<Person>{
+    private String name;
+    private String family;
+
+    public Person(String name, String family) {
+        this.name = name;
+        this.family = family;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+
+        int resultName;
+        resultName = name.compareTo(o.name);
+
+        return resultName==0 ? family.compareTo(o.family) : resultName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", family='" + family + '\'' +
+                '}';
+    }
+}
+
